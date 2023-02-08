49fada77885a0509ae5e6ceb7f283e7733cdbe74
Matthew Adams <matthew@matthewadams.me>
2023-02-08 15:33:25 -0600

    feat: support shortfall strategies

--- README.md
+++ README.md
@@ -34,11 +34,23 @@ Sample customized usage:
 
 > NOTE: In the above example, if your current data remaining in the pool plus the topup meets or exceeds the threshold,
 > then the number of gigabytes purchased will be the given value. However, if the the data remaining the pool does not
-> meet the threshold, then enough whole gigabytes are added to meet or just exceed the threshold.
+> meet the threshold, you need to tell the script what do to.
+
+If your `topup-gb` is not enough to get you at or over `threshold-gb`, the script will fail, unless you provide a strategy via
+the `--topup-shortfall-strategy`. Allowed values are:
+
+* `fail` (the default): causes the script to fail with a nonzero exit code.
+* `exit`: causes the script to do nothing and terminate with a zero exit code.
+* `retain`: leaves the `topup-gb` value unchanged and performs a topup, leaving you below `threshold-gb`, but not
+  costing you any more then you expect.
+* `increase`: increases `topup-gb` to the next whole gigabyte that, when added to your data pool balance, meets or
+  exceeds `threshold-gb`. This will cost you more money, obviously, so use it with care.
 
 ## Docker
-You can build your own Docker image with this repo's `Dockerfile`, until such time as I get images publishing automatically
-to https://hub.docker.com.  Usage is the same as the raw script usage.  For example:
+
+You can build your own Docker image with this repo's `Dockerfile`, until such time as I get images publishing
+automatically
+to https://hub.docker.com. Usage is the same as the raw script usage. For example:
 
 ```shell
 docker build -t poolminder:latest .
--- poolminder.sh
+++ poolminder.sh
@@ -14,11 +14,14 @@ THRESHOLD_GB=1
 TOPUP_GB=1
 TOKEN="$POOLMINDER_TOKEN"
 POOL_ID="$POOLMINDER_POOL_ID"
+AUTH_HEADER="USMAuthorization"
+TOPUP_SHORTFALL_STRATEGY=fail
 
 function usage() {
   printf "Usage: $0\n
   --token <token> REQUIRED: Your usmobile.com API token.\n\
     It's the JWT returned by (POST /web-gateway/api/v1/auth).\n\
+    Use your browser's dev tools to get it.\n\
     It can also be specified via environment variable POOLMINDER_TOKEN.\n\
   --pool-id <pool-id> REQUIRED: The id of the data pool you want to possibly top up.\n\
     Go to https://app.usmobile.com/dashboard/app/pools,\n\
@@ -30,7 +33,15 @@ function usage() {
   --topup-gb <topup> OPTIONAL, default 1: the number of gigabytes to top up the pool with.\n\
     Any fraction will be truncated.\n\
     If the pool's current balance plus this value doesn't achieve the threshold,\n\
-    it is increased to be the next whole number of gigabytes that meets or exceeds the threshold.\n\
+    the behavior is controlled by the strategy given by --topup-shortfall-strategy.\n\
+  --topup-shortfall-strategy <'fail' | 'exit' | 'retain' | 'increase'> OPTIONAL, default 'fail':\n\
+    the strategy to employ when the topup-gb does not achieve a balance greater than threshold-gb.\n\
+    Possible strategies are:\n\
+      'fail': the script terminates immediately with a nonzero exit code\n\
+      'exit': the script terminates immediately with a exit code of 0\n\
+      'retain': the current topup-gb value is retained and applied\n\
+      'increase': the current topup-gb value is increased to the next whole gb that meets or exceeds the threshold-gb value\n\
+  --verbose OPTIONAL: be verbose.\n\
   --help,-h OPTIONAL: produces this message.\n\
   \n\
   Any unrecognized option will cause this script to exit with a nonzero status.\n"
@@ -38,7 +49,6 @@ function usage() {
 
 while [ -n "$1" ]; do
   case "$1" in
-  # required args
   --token)
     shift
     TOKEN="$1"
@@ -47,12 +57,21 @@ while [ -n "$1" ]; do
     shift
     POOL_ID="$1"
     ;;
-  # optional args
   --dry-run)
     DRY_RUN=1
     ;;
   --no-dry-run)
     DRY_RUN=
+    ;;
+  --verbose)
+    VERBOSE=1
+    ;;
+  --no-verbose)
+    VERBOSE=
+    ;;
+  --topup-shortfall-strategy)
+    shift
+    TOPUP_SHORTFALL_STRATEGY="$1"
     ;;
   --threshold-gb)
     shift
@@ -78,8 +97,19 @@ done
 # scrub
 TOKEN="$(echo "$TOKEN" | xargs)"
 POOL_ID="$(echo "$POOL_ID" | xargs)"
-AUTH_HEADER="$(echo "$AUTH_HEADER" | xargs)"
 DRY_RUN="$(echo "$DRY_RUN" | xargs)"
+VERBOSE="$(echo "$VERBOSE" | xargs)"
+THRESHOLD_GB="$(echo "$THRESHOLD_GB" | xargs)"
+TOPUP_GB="$(echo "$TOPUP_GB" | xargs)"
+TOPUP_GB="${TOPUP_GB%%.*}" # truncate fraction
+TOPUP_SHORTFALL_STRATEGY="$(echo "$TOPUP_SHORTFALL_STRATEGY" | tr '[:upper:]' '[:lower:]' | xargs)"
+
+if [ -n "$VERBOSE" ]; then
+  echo "TOKEN=<masked>"
+  for it in POOL_ID DRY_RUN THRESHOLD_GB TOPUP_GB ALLOW_TOPUP_INCREASE; do
+    echo "$it=${!it}"
+  done
+fi
 
 # validate
 if [ -z "$TOKEN" ]; then
@@ -98,16 +128,38 @@ if [ "$(echo "$TOPUP_GB <= 0" | bc)" == 1 ]; then
   echo "topup-gb must be >= 0" >&2
   exit 3
 fi
+case "$TOPUP_SHORTFALL_STRATEGY" in
+fail | exit | retain | increase) ;;
+*)
+  echo "invalid topup-shortfall-strategy: $TOPUP_SHORTFALL_STRATEGY" >&2
+  exit 3
+  ;;
+esac
 
-TOPUP_GB="$(printf '%.0f' "$TOPUP_GB")" # truncate fraction
+if [ -n "$VERBOSE" ]; then
+  echo "TOPUP_GB after truncation=$TOPUP_GB"
+fi
+
+AUTH="$AUTH_HEADER:Bearer $TOKEN"
 BASE_URL=https://api.usmobile.com/web-gateway/api/v1
-
-AUTH="USMAuthorization:Bearer $TOKEN"
 POOL_DATA_URL="$BASE_URL/pools/$POOL_ID"
+if [ -n "$VERBOSE" ]; then
+  echo "POOL_DATA_URL=$POOL_DATA_URL"
+fi
 
 json="$(http $POOL_DATA_URL "$AUTH")"
 REMAINING_MB="$(echo "$json" | jq .balanceInMB)"
+if [ "$REMAINING_MB" == 'null' ]; then
+  set -e
+  echo "failed to get pool data; response: $(echo "$json" | jq)"
+  exit 4
+fi
+
 REMAINING_GB="$(echo "$REMAINING_MB / 1024" | bc --mathlib)"
+if [ -n "$VERBOSE" ]; then
+  echo "REMAINING_MB=$REMAINING_MB"
+  echo "REMAINING_GB=$REMAINING_GB"
+fi
 if [ "$(echo "$REMAINING_GB > $THRESHOLD_GB" | bc --mathlib)" == 1 ]; then
   echo "remaining gb of $REMAINING_GB Gb > $THRESHOLD_GB Gb; not topping up"
   exit 0
@@ -115,18 +167,50 @@ fi
 
 # double check TOPUP_GB
 if [ "$(echo "$REMAINING_GB + $TOPUP_GB < $THRESHOLD_GB" | bc --mathlib)" == 1 ]; then
-  TOPUP_GB="$(echo "$THRESHOLD_GB - $REMAINING_GB" | bc --mathlib)"
-  # now round up to the next whole gb
-  TOPUP_GB="$(printf '%.0f' $TOPUP_GB)" # floor
-  TOPUP_GB="$(echo "$TOPUP_GB + 1" | bc --mathlib)"
+  TOPUP_SHORTFALL_GB="$(echo "$THRESHOLD_GB - ($REMAINING_GB + $TOPUP_GB)" | bc --mathlib)"
+  if [ -n "$VERBOSE" ]; then
+    echo "TOPUP_GB short of threshold by $TOPUP_SHORTFALL_GB"
+  fi
+  case "$TOPUP_SHORTFALL_STRATEGY" in
+  fail)
+    echo "topup shortfall of $TOPUP_SHORTFALL_GB; failing based on strategy $TOPUP_SHORTFALL_STRATEGY" >&2
+    exit 4
+    ;;
+  exit)
+    echo "topup shortfall of $TOPUP_SHORTFALL_GB; exiting based on strategy $TOPUP_SHORTFALL_STRATEGY"
+    exit 0
+    ;;
+  retain)
+    echo "topup shortfall of $TOPUP_SHORTFALL_GB; topping up by only $TOPUP_GB based on strategy $TOPUP_SHORTFALL_STRATEGY"
+    ;;
+  increase)
+    # round up to the next whole gb
+    TOPUP_GB="${TOPUP_SHORTFALL_GB%%.*}"              # floor
+    TOPUP_GB="$(echo "$TOPUP_GB + 1" | bc --mathlib)" # +1
+    echo "topup shortfall of $TOPUP_SHORTFALL_GB; increasing topup to $TOPUP_GB based on strategy $TOPUP_SHORTFALL_STRATEGY"
+    ;;
+  *)
+    echo "unsupported topup strategy $TOPUP_SHORTFALL_STRATEGY" >&2
+    exit 4
+    ;;
+  esac
+
+  if [ -n "$VERBOSE" ]; then
+    echo "final TOPUP_GB=$TOPUP_GB"
+  fi
 fi
 
 TOPUP_URL="$POOL_DATA_URL/topUpAndBasePlan"
 CREDIT_CARD_TOKEN="$(echo "$json" | jq -r .creditCardToken)"
 
-cmd="http $TOPUP_URL '$AUTH' creditCardToken='$CREDIT_CARD_TOKEN' topUpSizeInGB='$TOPUP_GB'"
+if [ -n "$VERBOSE" ]; then
+  echo "TOPUP_URL=$TOPUP_URL"
+  echo "CREDIT_CARD_TOKEN=$CREDIT_CARD_TOKEN"
+fi
+
 if [ -n "$DRY_RUN" ]; then
-  echo "dry run -- would issue request: $cmd"
+  echo "dry run -- would issue request:"
+  echo "http $TOPUP_URL '$AUTH_HEADER:<masked>' creditCardToken='$CREDIT_CARD_TOKEN' topUpSizeInGB='$TOPUP_GB'"
   exit 0
 fi
 
